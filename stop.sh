ba0b1dfae35376b544810da3f5ea303bf7e18f19
Evening <johnchangqi@hotmail.com>
2023-07-14 17:04:05 +0800

    Delete stop.sh

--- stop.sh
+++
@@ -1,9 +0,0 @@
-echo "Stopping osu-data-docker"
-cd osu-data-docker || exit 1
-docker compose --profile files stop
-
-echo "Stopping osu-tools-docker"
-cd ../osu-tools-docker || exit 1
-docker compose stop
-
-exit 0
