d53033e270670ae6e6ec0341caf89e179dcf3428
Evening <johnchangqi@hotmail.com>
2023-07-23 11:46:24 +0800

    Refactor script to allow arguments from terminal

--- projects/sr-calc/input.sh
+++ projects/sr-calc/input.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-usage="$(basename "$0") [RUN_TAG] [-d DB_URL] [-f FILES_URL] [-o OSU_GIT] [-o OSU_GIT_BRANCH] [-t OSU_TOOLS_GIT] [-t OSU_TOOLS_GIT_BRANCH]
+usage="Usage: $(basename "$0") [-v MODE_VERSION] [-d DATA_DATE] [-r RUN_TAG] [-o OSU_GIT] [-o OSU_GIT_BRANCH] [-t OSU_TOOLS_GIT] [-t OSU_TOOLS_GIT_BRANCH] [-e ENV_PATH ]
+       $(basename "$0") [-y DATABASE_URL] [-z FILES_URL] [-r RUN_TAG] [-o OSU_GIT] [-o OSU_GIT_BRANCH] [-t OSU_TOOLS_GIT] [-t OSU_TOOLS_GIT_BRANCH] [-e ENV_PATH ]
 
-Argument:
-  RUN_TAG   Run tag is a name to identify the current run in future runs.
-            This will be used as the name of a directory.
-            E.g. my_experiment will create osu.view/sr-calc/my_experiment to store all results.
 
 Options:
   -h     Display this help.
-  -v     Game Mode and Version: {MODE}_{VERSION}
-         MODE    is either 'osu', 'mania', 'catch' or 'taiko'.
-         VERSION is either 'top_1000', 'top_10000' or 'random_10000'
-         E.g. 'osu_top_1000'.
-  -d     Date in the format YYYY_MM_DD.    Default: $(date '+%Y_%m_01') (1st of this Month)
-  -o (0) osu! game git link.               Default: https://github.com/ppy/osu
-  -o (1) osu! game git branch name.        Default: master
-  -t (0) osu! tools git link.              Default: https://github.com/ppy/osu-tools
-  -t (1) osu! tools git branch name.       Default: master
+  -v     MODE_VERSION: E.g. 'osu_top_1000'.
+         - MODE               is either 'osu', 'mania', 'catch' or 'taiko'.
+         - VERSION            is either 'top_1000', 'top_10000' or 'random_10000'
+  -r     RUN_TAG              identifier of this run       Default: $(date '+%Y_%m_%d_%H_%M_%S') (Current Date Time)
+  -d     DATA_DATE            in the format YYYY_MM_DD.    Default: $(date '+%Y_%m_01') (1st of this Month)
+  -o (0) OSU_GIT              osu! game git link.          Default: https://github.com/ppy/osu
+  -o (1) OSU_GIT_BRANCH       osu! game git branch name.   Default: master
+  -t (0) OSU_TOOLS_GIT        osu! tools git link.         Default: https://github.com/ppy/osu-tools
+  -t (1) OSU_TOOLS_GIT_BRANCH osu! tools git branch name.  Default: master
+  -e     ENV_PATH             path to .env file            Default: /tmp/sr-calc/.env
 
   Use these options if you need more control
   -y     https://data.ppy.sh Database URL. Example: https://data.ppy.sh/$(date '+%Y_%m_01')_performance_<VERSION>.tar.bz2
@@ -26,20 +24,20 @@ Options:
          Overrides -d
 
 Examples:
-  Use the most recent data ($(date '+%Y_%m_01')) for catch top 1000
+  Use the most recent data ($(date '+%Y_%m_01')) for catch top 1000. Name it 'my_run'.
 
-    $(basename "$0") my_experiment \\
+    $(basename "$0") -r my_run \\
       -v catch_top_1000
 
   Use a custom date. (It must exist in https://data.ppy.sh!)
 
-    $(basename "$0") my_experiment \\
+    $(basename "$0") -r my_run \\
       -v catch_top_1000 \\
       -d 2023_05_01
 
   Use a custom /osu and /osu-tools git
 
-    $(basename "$0") my_experiment \\
+    $(basename "$0") -r my_run \\
       -v catch_top_1000 \\
       -o https://github.com/Eve-ning/osu \\
       -o my_branch \\
@@ -48,23 +46,25 @@ Examples:
 
   Use a custom database and files URL. This will override any -v and -d options.
 
-    $(basename "$0") my_experiment \\
+    $(basename "$0") -r my_run \\
       -y https://github.com/Eve-ning/osu-data-docker/raw/master/rsc/YYYY_MM_DD_performance_mania_top_1000.tar.bz2 \\
       -z https://github.com/Eve-ning/osu-data-docker/raw/master/rsc/YYYY_MM_DD_osu_files.tar.bz2
 "
 
-while getopts "hv:d:o:t:y:z:" opt; do
+while getopts "hv:r:d:o:t:y:z:e:" opt; do
   case $opt in
   h)
     echo "$usage"
     exit
     ;;
   v) VERSION=$OPTARG ;;
+  r) RUN_TAG=$OPTARG ;;
   d) DATASET_DATE=$OPTARG ;;
   o) OSU_GIT_OPTS+=("$OPTARG") ;;
   t) OSU_TOOLS_GIT_OPTS+=("$OPTARG") ;;
   y) DB_URL=$OPTARG ;;
   z) FILES_URL=$OPTARG ;;
+  e) ENV_PATH=$OPTARG ;;
   *)
     echo "Invalid Argument"
     exit 1
@@ -73,6 +73,7 @@ while getopts "hv:d:o:t:y:z:" opt; do
 done
 
 # Normalize all values
+RUN_TAG=${RUN_TAG:="run_$(date '+%Y_%m_%d.%H_%M_%S')"}
 OSU_GIT=${OSU_GIT_OPTS[0]:='https://github.com/ppy/osu'}
 OSU_GIT_BRANCH=${OSU_GIT_OPTS[1]:='master'}
 OSU_TOOLS_GIT=${OSU_TOOLS_GIT_OPTS[0]:='https://github.com/ppy/osu-tools'}
@@ -80,59 +81,63 @@ OSU_TOOLS_GIT_BRANCH=${OSU_TOOLS_GIT_OPTS[1]:='master'}
 DATASET_DATE=${DATASET_DATE:="$(date '+%Y_%m_01')"}
 DB_URL=${DB_URL:="https://data.ppy.sh/${DATASET_DATE}_performance_${VERSION}.tar.bz2"}
 FILES_URL=${FILES_URL:="https://data.ppy.sh/${DATASET_DATE}_osu_files.tar.bz2"}
-
-# Check if all the inputs are correctly specified
+ENV_PATH=${ENV_PATH:="/tmp/sr-calc/.env"}
+# Input Validation
 BAD_INPUT=0
+echo "Input Validation: "
+echo -n "  RUN_TAG=$RUN_TAG: "
+if [ ! "$RUN_TAG" ]; then
+  echo -e "\e[31mRUN_TAG IS MISSING!\e[0m"
+  BAD_INPUT=1
+else echo -e "\e[32mOK\e[0m"; fi
 
-echo -n "Check validity of $DB_URL: "
+echo -n "  DB_URL=$DB_URL: "
 if ! curl --output /dev/null --silent --head --fail "$DB_URL"; then
   echo -e "\e[31mDOES NOT EXIST!\e[0m"
   BAD_INPUT=1
-else
-  echo ""
-fi
+else echo -e "\e[32mOK\e[0m"; fi
 
-echo -n "Check validity of $FILES_URL: "
+echo -n "  FILES_URL=$FILES_URL: "
 if ! curl --output /dev/null --silent --head --fail "$FILES_URL"; then
   echo -e "\e[31mDOES NOT EXIST!\e[0m"
   BAD_INPUT=1
-else
-  echo ""
-fi
+else echo -e "\e[32mOK\e[0m"; fi
 
-echo -n "Check validity of $OSU_GIT @ $OSU_GIT_BRANCH: "
+echo -n "  OSU_GIT @ OSU_GIT_BRANCH=$OSU_GIT @ $OSU_GIT_BRANCH: "
 git ls-remote --heads "${OSU_GIT}" "${OSU_GIT_BRANCH}" | grep "${OSU_GIT_BRANCH}" >/dev/null
 if [ "$?" == "1" ]; then
   echo -e "\e[31mDOES NOT EXIST!\e[0m"
   BAD_INPUT=1
-else
-  echo ""
-fi
+else echo -e "\e[32mOK\e[0m"; fi
 
-echo -n "Check validity of $OSU_TOOLS_GIT @ $OSU_TOOLS_GIT_BRANCH: "
+echo -n "  OSU_TOOLS_GIT @ OSU_TOOLS_GIT_BRANCH=$OSU_TOOLS_GIT @ $OSU_TOOLS_GIT_BRANCH: "
 git ls-remote --heads "${OSU_TOOLS_GIT}" "${OSU_TOOLS_GIT_BRANCH}" | grep "${OSU_TOOLS_GIT_BRANCH}" >/dev/null
 if [ "$?" == "1" ]; then
   echo -e "\e[31mDOES NOT EXIST!\e[0m"
   BAD_INPUT=1
-else
-  echo ""
-fi
+else echo -e "\e[32mOK\e[0m"; fi
+
+echo -n "  ENV_PATH=$ENV_PATH: "
+if [ ! "$ENV_PATH" ]; then
+  echo -e "\e[31mDOES NOT EXIST!\e[0m"
+  BAD_INPUT=1
+else echo -e "\e[32mOK\e[0m"; fi
 
 if [ "$BAD_INPUT" == "1" ]; then
-  echo -e "\e[31mInvalid Input. See above errors\e[0m"
-  echo "$usage"
+  echo -e "\e[31mInvalid Input. See above errors\e[0m. See usage with -h"
   exit 1
 fi
 
-# Print out the configuration
-echo "Configuration:
-  RUN_TAG=$RUN_TAG
-  DB_URL=$DB_URL
-  FILES_URL=$FILES_URL
-  OSU_GIT=$OSU_GIT
-  OSU_GIT_BRANCH=$OSU_GIT_BRANCH
-  OSU_TOOLS_GIT=$OSU_TOOLS_GIT
-  OSU_TOOLS_GIT_BRANCH=$OSU_TOOLS_GIT_BRANCH
-"
+mkdir -p "$(dirname "$ENV_PATH")"
 
+echo "RUN_TAG=$RUN_TAG
+DB_URL=$DB_URL
+FILES_URL=$FILES_URL
+OSU_GIT=$OSU_GIT
+OSU_GIT_BRANCH=$OSU_GIT_BRANCH
+OSU_TOOLS_GIT=$OSU_TOOLS_GIT
+OSU_TOOLS_GIT_BRANCH=$OSU_TOOLS_GIT_BRANCH" > "$ENV_PATH"
 
+source ./run.sh
+
+run
--- projects/sr-calc/run.sh
+++ projects/sr-calc/run.sh
@@ -24,117 +24,125 @@
 #                        /osu-data.env     osu-data  Environment config, will not exist if custom services are used.
 #                        /osu-tools.env    osu-tools "
 
-if [ $# -eq 0 ]; then
-  echo "Specify a directory to save files. ./run.sh [RUN_TAG]"
-  exit 1
-fi
-
-PROJ_DIR="$(pwd)"
-RESULT_DIR=osu.view/sr-calc/"$1"
-
-echo -n -e "\e[34mNavigating to Project Home: \e[0m"
-cd ../../
-pwd
-
-if [ -d "$RESULT_DIR" ]; then
-  read -r -p "$RESULT_DIR exists, overwrite it? [y/N]: " OVERWRITE
-  if [ "$OVERWRITE" == "Y" ] || [ "$OVERWRITE" == "y" ]; then
-    # Replace Directory if exists
-    echo "Accepted Overwrite for $RESULT_DIR"
-    rm -rf "$RESULT_DIR"
-  else
-    echo "Denied Overwrite, exit early"
-    exit 1
+check_overwrite() {
+  local OVERWRITE=""
+  local RUN_DIR="$1"
+  if [ -d "$RUN_DIR" ]; then
+    read -r -p "$RUN_DIR exists, overwrite it? [y/N]: " OVERWRITE
+    if [ "$OVERWRITE" == "Y" ] || [ "$OVERWRITE" == "y" ]; then
+      # Replace Directory if exists
+      echo "Accepted Overwrite for $RUN_DIR"
+      rm -rf "$RUN_DIR"
+    else
+      echo "Denied Overwrite, exit early"
+      exit 1
+    fi
   fi
-fi
-mkdir -p "$RESULT_DIR"
+  mkdir -p "$RUN_DIR"
+}
 
-echo ""
-echo "Pulling a large amount of .osu files can be slow."
-echo "Reuse .osu files by another result-set if the query is the same!"
-echo ""
-echo "Use an existing result-set's files or create a new result-set:"
+select_result_dir() {
+  echo ""
+  echo "Pulling a large amount of .osu files can be slow."
+  echo "Reuse .osu files by another result-set if the query is the same!"
+  echo ""
+  echo "Use an existing result-set's files or create a new result-set:"
 
-# The long find command finds all directories that have the `files` directory. This is necessary to get the files data
-# We firstly find all directories in sr-calc which contain the `files` dir
-# Then reverse it, so that `cut` can take the element from the back
-# Reverse it again after cut.
-select opt in "[NEW]" "[External Directory]" \
-  $(find osu.view/sr-calc/ -type d -name 'files' | rev | cut -d "/" -f 2 | rev); do
-  [ -n "${opt}" ] && break
-done
-
-# If New, we'll run the query, then extract matching files
-if [ "$opt" == "[NEW]" ]; then
-  FILES_DIR="$RESULT_DIR"/files
-  echo "Using new files dir $FILES_DIR"
+  local RUN_DIR="$1"
+  FILES_DIR=""
   CUSTOM_FILES=0
 
-# If External, we'll skip the query and file copying, we'll just run difficulty on this new dir.
-elif [ "$opt" == "[External Directory]" ]; then
-  echo If you use a relative directory, you are at "$(pwd)"
-  read -r -p "Your *.osu files directory: " EXT_FILES_DIR
-  while [ ! -d "$EXT_FILES_DIR" ]; do
-    echo "$EXT_FILES_DIR" is not a directory
-    read -r -p "Your *.osu files directory: " EXT_FILES_DIR
+  # The long find command finds all directories that have the `files` directory. This is necessary to get the files data
+  # We firstly find all directories in sr-calc which contain the `files` dir
+  # Then reverse it, so that `cut` can take the element from the back
+  # Reverse it again after cut.
+  select opt in "[NEW]" "[External Directory]" \
+    $(find "$RUN_DIR"/.. -type d -name 'files' | rev | cut -d "/" -f 2 | rev); do
+    [ -n "${opt}" ] && break
   done
-  echo "Found $(find "$EXT_FILES_DIR" -name "*.osu" | wc -l) *.osu files in $FILES_DIR"
-  FILES_DIR="$RESULT_DIR"/files
-  mkdir "$FILES_DIR"
-  echo "Copying over files from $EXT_FILES_DIR to $FILES_DIR/"
-  cp "$EXT_FILES_DIR"/*.osu "$FILES_DIR"/
-  CUSTOM_FILES=1
-# Else, we'll just use files from an existing result
-else
-  FILES_DIR="osu.view/sr-calc/${opt}/files"
-  CUSTOM_FILES=1
-  echo -e "\e[34mUsing custom files dir $FILES_DIR\e[0m"
-  if [ ! -d "$FILES_DIR" ]; then
-    echo "Files Directory does not exist."
-    exit 1
+
+  # If New, we'll run the query, then extract matching files
+  if [ "$opt" == "[NEW]" ]; then
+    FILES_DIR="$RUN_DIR"/files
+    echo "Using new files dir $FILES_DIR"
+    CUSTOM_FILES=0
+
+  # If External, we'll skip the query and file copying, we'll just run difficulty on this new dir.
+  elif [ "$opt" == "[External Directory]" ]; then
+    echo If you use a relative directory, you are at "$(pwd)"
+    read -r -p "Your *.osu files directory: " EXT_FILES_DIR
+    while [ ! -d "$EXT_FILES_DIR" ]; do
+      echo "$EXT_FILES_DIR" is not a directory
+      read -r -p "Your *.osu files directory: " EXT_FILES_DIR
+    done
+    echo "Found $(find "$EXT_FILES_DIR" -name "*.osu" | wc -l) *.osu files in $FILES_DIR"
+    FILES_DIR="$RUN_DIR"/files
+    mkdir "$FILES_DIR"
+    echo "Copying over files from $EXT_FILES_DIR to $FILES_DIR/"
+    cp "$EXT_FILES_DIR"/*.osu "$FILES_DIR"/
+    CUSTOM_FILES=1
+  # Else, we'll just use files from an existing result
+  else
+    FILES_DIR="$RUN_DIR/../${opt}/files"
+    CUSTOM_FILES=1
+    echo -e "\e[34mUsing custom files dir $FILES_DIR\e[0m"
+    if [ ! -d "$FILES_DIR" ]; then
+      echo "Files Directory does not exist."
+      exit 1
+    fi
   fi
-fi
 
-FILELIST_PATH="$RESULT_DIR"/filelist.txt
-NT_RESULTS_PATH="$RESULT_DIR"/nt.results.json
-DT_RESULTS_PATH="$RESULT_DIR"/dt.results.json
-HT_RESULTS_PATH="$RESULT_DIR"/ht.results.json
+}
 
-# Check if docker service osu.mysql is up
-if docker ps | grep -q osu.mysql && docker ps | grep -q osu.files; then
-  CUSTOM_SERVICES=1
-  echo -e "\e[33mServices are already running, will use current services.\e[0m"
-else
-  CUSTOM_SERVICES=0
-  echo -e "\e[33mServices not available, starting Services\e[0m"
+start_docker() {
+  local ENV_PATH="$1"
+  local CUSTOM_FILES="$2"
+
+  if [ "$CUSTOM_FILES" -eq 0 ]; then
+    docker compose \
+      --project-directory ./ \
+      --profile files \
+      -f "$PROJ_DIR"/docker-compose.yml \
+      --env-file ./osu-data-docker/.env \
+      --env-file ./osu-tools-docker/.env \
+      --env-file "$ENV_PATH" \
+      up --wait --build
+  else
+    docker compose \
+      --project-directory ./ \
+      --profile files \
+      -f "$PROJ_DIR"/docker-compose.yml \
+      --env-file ./osu-data-docker/.env \
+      --env-file ./osu-tools-docker/.env \
+      --env-file "$ENV_PATH" \
+      up --wait --build osu.tools
+  fi
+}
+
+stop_docker() {
+  local ENV_PATH=$1
   docker compose \
     --project-directory ./ \
     --profile files \
     -f "$PROJ_DIR"/docker-compose.yml \
     --env-file ./osu-data-docker/.env \
     --env-file ./osu-tools-docker/.env \
-    --env-file "$PROJ_DIR"/.env \
-    up --wait --build
+    --env-file "$ENV_PATH" \
+    stop
+}
 
-  echo "Configurations: "
-  echo " - /osu.git: $(grep OSU_GIT= "$PROJ_DIR"/.env | cut -d = -f 2 | head -1)"
-  echo "   - branch: $(grep OSU_GIT_BRANCH= "$PROJ_DIR"/.env | cut -d = -f 2 | head -1)"
-  echo " - /osu-tools.git: $(grep OSU_TOOLS_GIT= "$PROJ_DIR"/.env | cut -d = -f 2 | head -1)"
-  echo "   - branch: $(grep OSU_TOOLS_GIT_BRANCH= "$PROJ_DIR"/.env | cut -d = -f 2 | head -1)"
-fi
-
-# If we're not using custom files, we'll find them via query.
-if [ $CUSTOM_FILES -eq 0 ]; then
+run_query() {
   # Pull file list according to query
-  MYSQL_PASSWORD=p@ssw0rd1
+  local FILELIST_PATH="$1"
+  local FILES_DIR="$2"
+  local MYSQL_PASSWORD="$3"
   docker exec osu.mysql mysql -u root --password="$MYSQL_PASSWORD" \
     -D osu -N -e "$(cat "$PROJ_DIR"/query.sql)" \
     >"$FILELIST_PATH"
 
-  echo "File List saved in /osu.view/recalc-sr/$FILELIST_PATH"
-
   # Get osu.files dir name dynamically
-  OSU_FILES_DIRNAME=$(docker exec osu.files ls)
+  local OSU_FILES_DIRNAME=""
+  OSU_FILES_DIRNAME="$(docker exec osu.files ls)"
 
   # Create a temporary directory to copy all files to send to tar.
   echo "Moving Files to /$FILES_DIR/"
@@ -143,11 +151,42 @@ if [ $CUSTOM_FILES -eq 0 ]; then
     while read beatmap_id;
     do cp /osu.files/'"$OSU_FILES_DIRNAME"'/"$beatmap_id".osu /'"$FILES_DIR"'/"$beatmap_id".osu;
     done < /'"$FILELIST_PATH"';'
-fi
+}
 
-# Evaluate beatmaps via osu.tools
-docker exec osu.tools sh -c \
-  '
+copy_configs() {
+  local CUSTOM_FILES="$1"
+  local CUSTOM_SERVICES="$2"
+  local RUN_DIR="$3"
+  local PROJ_DIR="$4"
+  local ENV_PATH="$5"
+
+  if [ "$CUSTOM_FILES" -eq 1 ]; then
+    echo -e "\e[33mAs you used a custom fileset, query.sql will not be copied over.\e[0m"
+  else
+    cp "$PROJ_DIR"/query.sql "$RUN_DIR"/query.sql
+  fi
+
+  if [ "$CUSTOM_SERVICES" -eq 1 ]; then
+    echo -e "\e[33mAs you used a custom service, docker-compose.yml and .env files will not be copied over.\e[0m"
+  else
+    cp "$PROJ_DIR"/docker-compose.yml "$RUN_DIR"/
+    cp ./osu-data-docker/.env "$RUN_DIR"/osu-data.env
+    cp ./osu-tools-docker/.env "$RUN_DIR"/osu-tools.env
+    cp "$ENV_PATH" "$RUN_DIR"/sr-calc.env
+    echo "Stopping Services"
+    stop_docker $ENV_PATH
+  fi
+}
+
+evaluate_maps() {
+  local FILES_DIR="$1"
+  local NT_RESULTS_PATH="$2"
+  local DT_RESULTS_PATH="$3"
+  local HT_RESULTS_PATH="$4"
+
+  # Evaluate beatmaps via osu.tools
+  docker exec osu.tools sh -c \
+    '
   echo -n "Evaluating Beatmaps (Normal Time): ";
   dotnet PerformanceCalculator.dll difficulty "/'"$FILES_DIR"'" -j -o "/'"$NT_RESULTS_PATH"'" >> /dev/null;
   echo "Exported to '"$NT_RESULTS_PATH"'";
@@ -160,29 +199,40 @@ docker exec osu.tools sh -c \
   dotnet PerformanceCalculator.dll difficulty "/'"$FILES_DIR"'" -j -m ht -o "/'"$HT_RESULTS_PATH"'" >> /dev/null;
   echo "Exported to '"$HT_RESULTS_PATH"'";
   '
+}
 
-echo -e "\e[34mCopying Over Configurations for Lineage\e[0m"
-if [ $CUSTOM_FILES -eq 1 ]; then
-  echo -e "\e[33mAs you used a custom fileset, query.sql will not be copied over.\e[0m"
-else
-  cp "$PROJ_DIR"/query.sql "$RESULT_DIR"/query.sql
-fi
+run() {
+  RUN_DIR=osu.view/sr-calc/"$RUN_TAG"
+  PROJ_DIR="$(pwd)"
+  MYSQL_PASSWORD="p@ssw0rd1"
+  FILELIST_PATH="$RUN_DIR"/filelist.txt
+  NT_RESULTS_PATH="$RUN_DIR"/nt.results.json
+  DT_RESULTS_PATH="$RUN_DIR"/dt.results.json
+  HT_RESULTS_PATH="$RUN_DIR"/ht.results.json
 
-if [ $CUSTOM_SERVICES -eq 1 ]; then
-  echo -e "\e[33mAs you used a custom service, docker-compose.yml and .env files will not be copied over.\e[0m"
-else
-  cp "$PROJ_DIR"/docker-compose.yml "$RESULT_DIR"/
-  cp ./osu-data-docker/.env "$RESULT_DIR"/osu-data.env
-  cp ./osu-tools-docker/.env "$RESULT_DIR"/osu-tools.env
-  cp "$PROJ_DIR"/.env "$RESULT_DIR"/sr-calc.env
-  echo "Stopping Services"
-  docker compose \
-    --project-directory ./ \
-    --profile files \
-    -f "$PROJ_DIR"/docker-compose.yml \
-    --env-file ./osu-data-docker/.env \
-    --env-file ./osu-tools-docker/.env \
-    stop
-fi
+  echo -n -e "\e[34mNavigating to Project Home: \e[0m"
+  cd ../../
+  pwd
 
-echo -e "\e[32mCompleted.\e[0m"
+  check_overwrite "$RUN_DIR"
+  select_result_dir "$RUN_DIR"
+
+  if docker ps | grep -q osu.mysql && docker ps | grep -q osu.files; then
+    CUSTOM_SERVICES=1
+    echo -e "\e[33mServices are already running, will use current services.\e[0m"
+  else
+    CUSTOM_SERVICES=0
+    start_docker "$ENV_PATH" "$CUSTOM_FILES"
+  fi
+
+  if [ "$CUSTOM_FILES" -eq 0 ]; then
+    run_query "$FILELIST_PATH" "$FILES_DIR" "$MYSQL_PASSWORD"
+  fi
+
+  evaluate_maps "$FILES_DIR" "$NT_RESULTS_PATH" "$DT_RESULTS_PATH" "$HT_RESULTS_PATH"
+
+  echo -e "\e[34mCopying Over Configurations for Lineage\e[0m"
+  copy_configs "$CUSTOM_FILES" "$CUSTOM_SERVICES" "$RUN_DIR" "$PROJ_DIR" "$ENV_PATH"
+
+  echo -e "\e[32mCompleted.\e[0m"
+}
